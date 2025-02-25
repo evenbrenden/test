938e55b177286c31819abdb6c12bba9e5b1b6493
Even Brenden <evenbrenden@users.noreply.github.com>
2025-02-25 16:27:09 +0100

    Move screen locking home

---
+++ home/screen-locking.nix
@@ -0,0 +1,40 @@
+{ pkgs, ... }:
+
+{
+  services = {
+    # https://github.com/nix-community/home-manager/pull/6534
+    screen-locker = {
+      enable = true;
+      lockCmd = ''
+        ${pkgs.lib.getExe' pkgs.coreutils "env"} \
+        XSECURELOCK_DISCARD_FIRST_KEYPRESS=0 \
+        XSECURELOCK_KEY_XF86Display_COMMAND='${pkgs.autorandr}/bin/autorandr --force --change clone-largest' \
+        XSECURELOCK_KEY_F7_COMMAND='${pkgs.autorandr}/bin/autorandr --force --change clone-largest' \
+        ${pkgs.xsecurelock}/bin/xsecurelock
+      '';
+      xss-lock.extraOptions = [ "--transfer-sleep-lock" ];
+    };
+    # https://github.com/nix-community/home-manager/pull/6533
+    # Temporarily disable with: systemctl --user stop xidlehook.service
+    xidlehook = {
+      enable = true;
+      timers = [{
+        command = "${pkgs.systemd}/bin/loginctl lock-session $XDG_SESSION_ID";
+        delay = 15 * 60;
+      }];
+      not-when-fullscreen = true;
+    };
+  };
+  systemd.user.services.disable-xset-s-and-dpms = {
+    Unit = {
+      Description = "Disable X Screen Saver and DPMS";
+      After = [ "xss-lock.service" ];
+    };
+    Service = let xset = "${pkgs.lib.getExe pkgs.xorg.xset}";
+    in {
+      Type = "oneshot";
+      ExecStart = "${pkgs.lib.getExe pkgs.bash} -c '${xset} s off && ${xset} -dpms'";
+    };
+    Install = { WantedBy = [ "xss-lock.service" ]; };
+  };
+}
